c2c49b38209444b6217e758ae6d8255f758249c2
yash.devikar00 <yash.devikar00@gmail.com>
2018-12-27 10:53:12 +0530

    errors removed

--- load_data.py
+++ load_data.py
@@ -20,19 +20,8 @@ def get_all_pixels_labels(label):
             if label[i, j] not in max:
                 max.append(label[i, j])
     return sorted(max)
+    
 
-def divide_images_into_small_parts(image=None):
-    if image is None:
-        raise Exception("Image given to divide_images_into_small_parts function cannot be None")
-    image_height = image.shape[1]
-    image_width = image.shape[2]
-    
-    # trying to divide image in 4 folds
-    a1 = np.zeros((image.shape[0], int(image_height/2), int(image_width/2) ))
-    a2 = np.zeros((image.shape[0], int(image_height/2), int(image_width/2) ))
-    a3 = np.zeros((image.shape[0], int(image_height/2), int(image_width/2) ))
-    a4 = np.zeros((image.shape[0], int(image_height/2), int(image_width/2) ))
-    
     
 def sanity_check_for_label(label):
     # for a check wether the label is correctly modified or not. 
@@ -48,6 +37,8 @@ def sanity_check_for_label(label):
                     a[i, j] = label[i,j,k]
     plt.imshow(a)
     plt.show()
+
+
 def normalized(rgb):
     # Normalises Histogram. Function directly copied from opencv official tutorials
     # from opencv website.
@@ -59,11 +50,16 @@ def normalized(rgb):
     norm[:,:,1]=cv2.equalizeHist(g)
     norm[:,:,2]=cv2.equalizeHist(r)
     return norm
+
+
 def preprocess_label(label):
     for i in range(label.shape[0]):
         for j in range(label.shape[1]):
             label[i, j] = label[i, j]//32
     return one_hot_it(label)
+
+
+
 def one_hot_it(label):
     # Does one hot encoding of the images. Seperates various 
     # pixel values from the labels and converts the 2D array 
@@ -76,6 +72,8 @@ def one_hot_it(label):
         for j in range(label.shape[1]):
             cropped_label[i, j, label[i,j]] = label[i, j]
     return cropped_label
+
+
 def get_file_path(indice=1):
     parent_dir = Path("data")
     instrument_dir_path = os.path.join(parent_dir, 'cropped_train', 'instrument_dataset_{}'.format(indice))
@@ -85,6 +83,8 @@ def get_file_path(indice=1):
         return images_path, labels_path
     else:
         raise Exception("Images path doesnt exist")
+
+
 def get_one_data(indice, index):
     images_path, labels_path = get_file_path(indice)
     
@@ -96,6 +96,8 @@ def get_one_data(indice, index):
     label = np.array(cv2.imread(label, cv2.IMREAD_GRAYSCALE))
     label = preprocess_label(label)
     return image, label
+
+
 def get_more_data(indice=1, sample_size=1):
     if indice>4 or indice<0:
         raise Exception("Indice of instrument folder is invalid")
@@ -117,7 +119,8 @@ def get_more_data(indice=1, sample_size=1):
     return np.array(img), np.array(lbl)
 
 
+
+
 if __name__ == '__main__':
-    # img, lbl = get_more_data(1, 5)
-    # print(img.shape, lbl.shape)
-    divide_images_into_small_parts(np.zeros((3, 1024, 1280)))
+    img, lbl = get_more_data(1, 5)
+    print(img.shape, lbl.shape)
